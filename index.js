a2244e81bcc222dbbccb3fc805f1cc8c2cd044b4
haris-nine <87892644+haris-nine@users.noreply.github.com>
2026-06-20 18:31:47 +0500

    feat: add code for server

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("hello world?");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const routes = {
+  "/": "index.html",
+  "/about": "about.html",
+  "/contact": "contact-me.html",
+};
+
+const server = http.createServer((req, res) => {
+  const fileName = routes[req.url];
+
+  if (!fileName) {
+  const filePath = path.join(__dirname, "404.html");
+
+  fs.readFile(filePath, (err, data) => {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end(data);
+  });
+
+  return;
+}
+
+  const filePath = path.join(__dirname, fileName);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Internal Server Error");
+      return;
+    }
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
+});
+
+server.listen(8080, () => {
+  console.log("Server running at http://localhost:8080");
+});
